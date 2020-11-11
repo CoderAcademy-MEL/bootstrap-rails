203e51046cc7b3218f7203fef48e9daf412cae60
Edward Deam <edwardcrdeam@gmail.com>
2020-11-11 11:03:40 +1100

    lesson code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :error, :success
+
+  layout :layout_for_resource
+
+  protected
+
+  def layout_for_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
 end
